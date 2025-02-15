a33495ad41393a1326937474cc7f35e289105515
James Leahy <defuncart@gmail.com>
2025-02-15 12:52:26 +0100

    Migrate l10n

--- lib/core/ui/my_app.dart
+++ lib/core/ui/my_app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:game_saves_backup/core/l10n/app_localizations.dart';
 import 'package:game_saves_backup/core/theme/themes.dart';
 import 'package:game_saves_backup/features/home/ui/home_screen.dart';
 
